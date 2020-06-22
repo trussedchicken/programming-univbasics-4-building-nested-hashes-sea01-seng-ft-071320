974dd95440d9a5cc0a12fe4c19a1939f364b4cf1
Jeremy Winn <jeremyjwinn@gmail.com>
2020-06-22 17:52:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,6 +18,7 @@ def monopoly_with_third_tier
                                        :four_pieces_owned => 200
                                       },
                   :names => {:reading_railroad => {},
+                             :pennsylvania_railroad{},
                              :
                   }
   }}
